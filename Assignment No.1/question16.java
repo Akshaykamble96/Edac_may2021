1e6ea9d1951afc60eccaa00bbae7c9d0e4f76dc2
Akshaykamble96 <83824265+Akshaykamble96@users.noreply.github.com>
2021-05-09 19:24:44 +0530

    Add files via upload

---
+++ Assignment No.1/question16.java
@@ -0,0 +1,13 @@
+//Face pattern
+class Question16
+{
+	public static void main(String args[])
+	{
+		System.out.print    ("   +\"\"\"\"\"+ \n"
+               +"  [| o o |] \n"
+               +"   |  ^  | \n"
+               +"   | '-' | \n"
+               +"   +-----+");
+	}
+}
+		
